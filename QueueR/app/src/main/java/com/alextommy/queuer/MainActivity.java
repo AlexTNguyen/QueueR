60a0088d0b70b67eefc19f1f297f708eb9683fc0
AlexTNguyen <alex.nguyen@tufts.edu>
2016-09-29 02:26:18 -0400

    Added functionality to add button

--- QueueR/app/src/main/java/com/alextommy/queuer/MainActivity.java
+++ QueueR/app/src/main/java/com/alextommy/queuer/MainActivity.java
@@ -8,13 +8,25 @@ import com.alextommy.queuer.R.id;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomerAdapter adapter;
+    private View popupView;
+    private PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +49,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addEntry(View view) {
-        adapter.insert(new Customer("Alex", Integer.toString(2), "7:00 PM"));
+        EditText name   = (EditText)popupView.findViewById(R.id.nameEntry);
+        EditText size   = (EditText)popupView.findViewById(R.id.sEntry);
+        EditText time   = (EditText)popupView.findViewById(R.id.timeEntry);
+        adapter.insert(new Customer(name.getText().toString(),
+                                    size.getText().toString(),
+                                    time.getText().toString()));
         ListView listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
+        popupWindow.dismiss();
+    }
+
+    public void showPopup(View view) {
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        popupView = layoutInflater.inflate(R.layout.popup,
+                (ViewGroup) findViewById(R.id.popup));
+        popupWindow = new PopupWindow(popupView, 300, 400, true);
+        popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+        Log.v("pls", "workkkk");
     }
 }
