35a64507b4be43c8b496197b822f6ab5d1889f41
AlexTNguyen <alex.nguyen@tufts.edu>
2016-10-13 13:39:15 -0400

    added the qr scanner

--- QueueR/app/src/main/java/com/alextommy/queuer/EntryList.java
+++ QueueR/app/src/main/java/com/alextommy/queuer/EntryList.java
@@ -1,5 +1,6 @@
 package com.alextommy.queuer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,15 +20,24 @@ import java.util.List;
 
 public class EntryList extends AppCompatActivity {
 
-    private CustomerAdapter adapter;
+    private CustomerAdapter adapter = new CustomerAdapter(this, Customer.makeData());
     private View popupView;
     private PopupWindow popupWindow;
+    private static ListView listView;
     int temp_pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        if(intent.getStringExtra("add").equals("true")) {
+            String name = intent.getStringExtra("name");
+            String size = intent.getStringExtra("size");
+            String time = intent.getStringExtra("time");
+            adapter.insert(new Customer(name, size, time));
+        }
 
         TextView columnHeader1 = (TextView) findViewById(R.id.column_header1);
         TextView columnHeader2 = (TextView) findViewById(R.id.column_header2);
@@ -37,14 +47,13 @@ public class EntryList extends AppCompatActivity {
         columnHeader2.setText("Party Size");
         columnHeader3.setText("Wait Time");
 
-        ListView listView = (ListView) findViewById(R.id.listview);
-        List<Customer> list = Customer.makeData();
-        adapter = new CustomerAdapter(this, list);
-
+        listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(onListClick);
-
     }
+
+
+
 
     public void addEntry(View view) {
         EditText name   = (EditText)popupView.findViewById(R.id.nameEntry);
@@ -65,27 +74,21 @@ public class EntryList extends AppCompatActivity {
                 (ViewGroup) findViewById(R.id.popup));
         popupWindow = new PopupWindow(popupView, 600, 800, true);
         popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
-        // Log.v("pls", "work");
-        setContentView(R.layout.activity_main);
+        // Log.v("pls", "work")
     }
 
-    public void showList(View view) {
-        setContentView(R.layout.activity_main);
-        TextView columnHeader1 = (TextView) findViewById(R.id.column_header1);
-        TextView columnHeader2 = (TextView) findViewById(R.id.column_header2);
-        TextView columnHeader3 = (TextView) findViewById(R.id.column_header3);
-
-        columnHeader1.setText("Name");
-        columnHeader2.setText("Party Size");
-        columnHeader3.setText("Wait Time");
-
-        ListView listView = (ListView) findViewById(R.id.listview);
-        List<Customer> list = Customer.makeData();
-        adapter = new CustomerAdapter(this, list);
-
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(onListClick);
+    public CustomerAdapter getAdapter() {
+        return adapter;
     }
+
+    public void setAdapter(CustomerAdapter newAdapter){
+        adapter = newAdapter;
+    }
+//    public static void addEntryHelper (String name, String size, String time) {
+//        CustomerAdapter newAdapter = getAdapter();
+//        adapter.insert(new Customer(name, size, time));
+//        listView.setAdapter(adapter);
+//    }
 
     public void editEntry(View view) {
         // edit the list content
--- QueueR/app/src/main/java/com/alextommy/queuer/MainActivity.java
+++ QueueR/app/src/main/java/com/alextommy/queuer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.alextommy.queuer;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 
 
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.alextommy.queuer.R.id;
 
 import android.app.Activity;
@@ -23,21 +28,93 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.view.Menu;
 import android.content.Intent;
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView mScannerView;
+    private View popupView;
+    private PopupWindow popupWindow;
+    private boolean on = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.title);
 
-
     }
 
     public void showList(View view) {
         Intent intent = new Intent(this, EntryList.class);
+        intent.putExtra("add", "false");
         startActivity(intent);
 
     }
+
+    public void addEntry(View view) {
+        mScannerView.stopCamera();
+        String name   = ((EditText)popupView.findViewById(R.id.nameEntry)).getText().toString();
+        String size   = ((EditText)popupView.findViewById(R.id.sEntry)).getText().toString();
+        String time   = ((EditText)popupView.findViewById(R.id.timeEntry)).getText().toString();
+        Intent intent = new Intent(this, EntryList.class);
+        intent.putExtra("add", "true");
+        intent.putExtra("name", name);
+        intent.putExtra("size", size);
+        intent.putExtra("time", time);
+        popupWindow.dismiss();
+        startActivity(intent);
+    }
+
+    public void showPopup(View view) {
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        popupView = layoutInflater.inflate(R.layout.popup,
+                (ViewGroup) findViewById(R.id.popup));
+        popupWindow = new PopupWindow(popupView, 600, 800, true);
+        popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+        // Log.v("pls", "work")
+    }
+
+    public void showPopup() {
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext()
+                .getSystemService(LAYOUT_INFLATER_SERVICE);
+        popupView = layoutInflater.inflate(R.layout.popup,
+                (ViewGroup) findViewById(R.id.popup));
+        popupWindow = new PopupWindow(popupView, 600, 800, true);
+        popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+        // Log.v("pls", "work")
+    }
+
+    public void scanQR(View view){
+        mScannerView = new ZXingScannerView(this); // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        on = true;
+        mScannerView.startCamera(); // Start camera
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showPopup();
+            }
+        }, 3000);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(on != false) {
+            mScannerView.stopCamera(); // Stop camera on pause
+            setContentView(R.layout.title);
+        }
+        on = false;
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        showPopup();
+    }
+
+
 
 }
